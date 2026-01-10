3028e2e436aec772230664b3acd684fc4120a940
MarianDev22 <marianmfalicante@gmail.com>
2026-01-10 13:40:47 +0100

    WI: core components

---
+++ src/core/components/navbar/navbar.tsx
@@ -0,0 +1,37 @@
+//import type { MenuOption } from "@core/types/menu-option";
+
+//import { Link } from "react-router";
+
+// type Props = {
+//     readonly options: MenuOption[];
+// };
+
+// export const Menu: React.FC<Props> = ({ options }) => {
+//     return (
+//         <nav>
+//             <ul>
+//                 {options.map((item) => (
+//                     <li key={item.path}>
+//                         <Link to={item.path}> {item.label}</Link>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </nav>
+//     );
+// };
+
+
+
+export const Menu: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                
+                    <li >
+                       
+                    </li>
+               
+            </ul>
+        </nav>
+    );
+};
