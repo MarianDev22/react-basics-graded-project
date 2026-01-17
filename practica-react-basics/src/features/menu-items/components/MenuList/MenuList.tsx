b48915e4e7afb7b9ac5450e975988b96b1f32fc8
MarianDev22 <marianmfalicante@gmail.com>
2026-01-17 20:14:26 +0100

    feat: conected to api - getMenuList ok

--- practica-react-basics/src/features/menu-items/components/MenuList/MenuList.tsx
+++ practica-react-basics/src/features/menu-items/components/MenuList/MenuList.tsx
@@ -1,11 +1,31 @@
 import type React from "react";
 import method from "@features/menu-items/services/products-service"
+import { useEffect, useState } from "react";
+import type { MenuItem } from "@features/menu-items/types/menu-item";
+import { Card } from "@core/components/card/card";
 
 export const MenuList: React.FC = () =>{
-    
+    const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+
+    useEffect(() => {
+        const loadList =  async (): Promise<void> => {
+            try {
+                const data = await method.getMenuList()
+                setMenuItems(data);
+                
+            } catch (error) {
+                alert("error")
+            }
+        }
+        loadList();
+    }, []);
     
     return(
-        <h2>Listado de los platos del menú</h2>
+        <>
+        {menuItems.map((item) => (
+        <Card key={item.id}>{item.name}</Card>
+        ))}        
+        </>
     )
 }
 
